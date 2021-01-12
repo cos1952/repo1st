ff05a39617a55d37d05c5613ad0fc11ed35ee2dc
Cos Constantinoy <cc99@ntrs.com>
2021-01-12 14:47:37 +0000

    added something

---
+++ exercise_1.py
@@ -0,0 +1,18 @@
+operator = input( "please enter an operator")
+print(operator)
+number_1 = input( "please enter a number")
+number_2 = input( "please enter an another number")
+print (number_1)
+print (number_2)
+
+if operator == "+" :
+    print (int(number_1) + int(number_2))
+elif operator == "-":
+    print (int(number_1) - int(number_2))
+elif operator == "*":
+    print (int(number_1) * int(number_2))
+
+
+
+
+
---
+++ testrun.py
@@ -0,0 +1,8 @@
+if True:
+    print('First if statement')
+if False:
+    print('Second if statement')
+if 1 + 2 == 3:
+    print('Third if statement')
+if 1 > 2 or 3 < 4:
+    print('Fourth if statement')
